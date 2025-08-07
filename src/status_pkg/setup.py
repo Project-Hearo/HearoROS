4cf06e3dcb744a96510907d4fb2b3ed5053f5676
rkrzy <81816761+rkrzy@users.noreply.github.com>
2025-08-07 20:23:54 +0900

    feat: setup.py 설정 (#3)

--- src/status_pkg/setup.py
+++ src/status_pkg/setup.py
@@ -22,6 +22,7 @@ setup(
         'console_scripts': [
             'ip_publisher = status_pkg.ip_publisher:main',
             'ble_provision_server = status_pkg.ble_provision_server:main',
+            'battery_imu_service = status_pkg.battery_imu_service_node:main'
         ],
     },
 )
