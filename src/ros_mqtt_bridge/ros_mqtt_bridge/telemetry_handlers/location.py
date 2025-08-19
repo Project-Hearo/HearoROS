234a0db16d0af8afe7dab5f32242eb7ec232c8bf
rkrzy <81816761+rkrzy@users.noreply.github.com>
2025-08-19 22:01:11 +0900

    feat: x, y, z 전달 좌표 변환  추가(#4)

--- src/ros_mqtt_bridge/ros_mqtt_bridge/telemetry_handlers/location.py
+++ src/ros_mqtt_bridge/ros_mqtt_bridge/telemetry_handlers/location.py
@@ -3,6 +3,8 @@ from geometry_msgs.msg import PointStamped
 from .base import TelemetryHandler
 from .registry import register_telemetry
 
+from tf2_ros import Buffer, TransformListener
+
 @register_telemetry
 class LocationTelemetry(TelemetryHandler):
     name = "location"
@@ -10,6 +12,8 @@ class LocationTelemetry(TelemetryHandler):
     def __init__(self, node, *, rate_hz: float = 2.0):
         super().__init__(node, rate_hz=rate_hz)
         self.sub = None
+        self.tf_buffer = Buffer()
+        self.tf_listener = TransformListener(self.tf_buffer, self)
     
     def start(self):
         if self._running: return
@@ -25,9 +29,16 @@ class LocationTelemetry(TelemetryHandler):
             self._running = False
     def callback(self, msg: PointStamped):
         if not self._tick(): return
-        self.node._publish_telemetry("location", {
-            "x":msg.point.x, "y": msg.point.y, "z":msg.point.z,
-            "frame_id":msg.header.frame_id,
-        })
+        try: 
+            point_in_map = self.tf_buffer.transform(msg, 'map')
+            self.node._publish_telemetry(
+                "location", {
+                "x":point_in_map.point.x, 
+                "y": point_in_map.point.y, 
+                "z":point_in_map.point.z,
+                "frame_id":"map",
+            })
+        except Exception as e:
+            self.get_logger().warn(f"Tranform failed: {e}")
         
     
