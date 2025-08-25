ad52a018d997b75097dd4ceef5c53ab7ea3b600a
rkrzy <81816761+rkrzy@users.noreply.github.com>
2025-08-25 22:03:10 +0900

    feat: 오타 수정

--- hearo_docker/ros2_humble_mounted.sh
+++ hearo_docker/ros2_humble_mounted.sh
@@ -14,7 +14,7 @@ docker run -it --rm \
   -v /dev/input:/dev/input \
   -v /dev/video0:/dev/video0 \
   -v /var/run/dbus:/var/run/dbus \
-  -v /dev/dri:/dev/dri \        
+  -v /dev/dri:/dev/dri \
   -v ~/Desktop/HearoROS:/root/HearoROS \
   hearo/ros-humble-slam:latest \
   /bin/bash /root/1.sh          
