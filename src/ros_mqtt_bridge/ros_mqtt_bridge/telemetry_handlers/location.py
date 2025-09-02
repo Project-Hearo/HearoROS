a91504b7bd6bf0d9855cba3827f16b869e35b2af
rkrzy <81816761+rkrzy@users.noreply.github.com>
2025-09-02 20:53:13 +0900

    feat: 좌표발행 handler 수정(#4)

--- src/ros_mqtt_bridge/ros_mqtt_bridge/telemetry_handlers/location.py
+++ src/ros_mqtt_bridge/ros_mqtt_bridge/telemetry_handlers/location.py
@@ -1,8 +1,8 @@
-import json, time
+import rclpy
 from geometry_msgs.msg import PointStamped
 from .base import TelemetryHandler
 from .registry import register_telemetry
-
+from tf2_geometry_msgs import do_transform_point
 from tf2_ros import Buffer, TransformListener
 
 @register_telemetry
@@ -13,24 +13,31 @@ class LocationTelemetry(TelemetryHandler):
         super().__init__(node, rate_hz=rate_hz)
         self.sub = None
         self.tf_buffer = Buffer()
-        self.tf_listener = TransformListener(self.tf_buffer, self)
+        self.tf_listener = TransformListener(self.tf_buffer, self.node)
+        self.running = False
     
     def start(self):
-        if self._running: return
-        self._running = True
+        if self.running:
+            return
+        self.running = True
         self.sub = self.node.create_subscription(PointStamped, '/robot/location', self.callback, 10)
         
     def stop(self):
-        if not self._running: return
+        if not self.running: return
         try: 
-            if self.sub: self.node.destroy_subscription(self.sub)
+            if self.sub: 
+                self.node.destroy_subscription(self.sub)
         finally:
             self.sub = None
-            self._running = False
+            self.running = False
+            
     def callback(self, msg: PointStamped):
-        if not self._tick(): return
+        if not self.tick(): return
         try: 
-            point_in_map = self.tf_buffer.transform(msg, 'map')
+            tf = self.tf_buffer.lookup_transform(
+                'map', msg.header.frame_id, rclpy.time.Time()
+            )
+            point_in_map = do_transform_point(msg, tf)
             self.node._publish_telemetry(
                 "location", {
                 "x":point_in_map.point.x, 
