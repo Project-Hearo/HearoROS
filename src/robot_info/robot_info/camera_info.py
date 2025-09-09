781a877ad67e956e5ac01e34b20622c6b9914d40
rkrzy <81816761+rkrzy@users.noreply.github.com>
2025-09-09 21:11:31 +0900

    test: 기존의 코드 성능 측정을 위해서 추가

--- src/robot_info/robot_info/camera_info.py
+++ src/robot_info/robot_info/camera_info.py
@@ -1,64 +1,92 @@
-import copy
-import rclpy
-from rclpy.node import Node
-from sensor_msgs.msg import Image
-from rclpy.qos import QoSProfile, ReliabilityPolicy, HistoryPolicy, DurabilityPolicy
+#!/usr/bin/env python3
+# encoding: utf-8
+import rospy
+import time
+import cv2 as cv
+import numpy as np
+import mediapipe as mp
+from geometry_msgs.msg import Point
+from yahboomcar_msgs.msg import PointArray
 
-class CameraInfo(Node):
-    def __init__(self):
-        super().__init__('camera_info')
-        self.declare_parameter('input_topic', '/image_raw')
-        self.declare_parameter('output_topic', '/image_raw_refine')
-        self.declare_parameter('rate_hz', 10.0)
-        
-        input_topic = self.get_parameter('input_topic').get_parameter_value().string_value
-        output_topic = self.get_parameter('output_topic').get_parameter_value().string_value
-        rate_hz = float(self.get_parameter('rate_hz').get_parameter_value().double_value)
+class HandDetector:
+    def __init__(self, mode=False, maxHands=2, detectorCon=0.5, trackCon=0.5):
+        self.mpHand = mp.solutions.hands
+        self.mpDraw = mp.solutions.drawing_utils
+        self.hands = self.mpHand.Hands(
+            static_image_mode=mode,
+            max_num_hands=maxHands,
+            min_detection_confidence=detectorCon,
+            min_tracking_confidence=trackCon )
+        self.pub_point = rospy.Publisher('/mediapipe/points', PointArray, queue_size=1000)
+        self.lmDrawSpec = mp.solutions.drawing_utils.DrawingSpec(color=(0, 0, 255), thickness=-1, circle_radius=6)
+        self.drawSpec = mp.solutions.drawing_utils.DrawingSpec(color=(0, 255, 0), thickness=2, circle_radius=2)
 
-        sensor_qos = QoSProfile(
-            depth=1,
-            reliability=ReliabilityPolicy.BEST_EFFORT,
-            durability=DurabilityPolicy.VOLATILE,
-            history=HistoryPolicy.KEEP_LAST
-        )
-        
-        self.sub = self.create_subscription(Image, input_topic, self.callback, sensor_qos)
-        self.pub = self.create_publisher(Image, output_topic, sensor_qos)
+    def pubHandsPoint(self, frame, draw=True):
+        pointArray = PointArray()
+        img = np.zeros(frame.shape, np.uint8)
+        img_RGB = cv.cvtColor(frame, cv.COLOR_BGR2RGB)
+        t0 = time.time()
+        self.results = self.hands.process(img_RGB)
+        t1 = time.time()
+        print("mediapipe process time (ms):", (t1-t0) * 1000)
+        if self.results.multi_hand_landmarks:
+            '''
+            draw_landmarks():Draw landmarks and connections on the image.
+            image: A three-channel RGB image represented as a numpy ndarray.
+            landmark_list: Raw message of a list of normalized landmarks to be annotated on the image.
+            connections: A list of placemark index tuples specifying how to connect placemarks in the drawing.
+            landmark_drawing_spec: Lets you specify drawing settings for a placemark, such as color, line weight, and circle radius.
+            connection_drawing_spec: Lets you specify the connection's drawing settings, such as color and line thickness.
+            '''
+            for i in range(len(self.results.multi_hand_landmarks)):
+                if draw: self.mpDraw.draw_landmarks(frame, self.results.multi_hand_landmarks[i], self.mpHand.HAND_CONNECTIONS, self.lmDrawSpec, self.drawSpec)
+                self.mpDraw.draw_landmarks(img, self.results.multi_hand_landmarks[i], self.mpHand.HAND_CONNECTIONS, self.lmDrawSpec, self.drawSpec)
+                for id, lm in enumerate(self.results.multi_hand_landmarks[i].landmark):
+                    point = Point()
+                    point.x, point.y, point.z = lm.x, lm.y, lm.z
+                    pointArray.points.append(point)
+        self.pub_point.publish(pointArray)
+        return frame, img
 
-        self._period = 1.0 / max(0.1, rate_hz)
-        self._next_pub = 0.0
-        
-    def _now_sec(self) -> float:
-        t = self.get_clock().now().to_msg()
-        return t.sec + t.nanosec * 1e-9
-    
-    def callback(self, msg:Image):
-        
-        
-        now = self._now_sec()
-        
-        if now < self._next_pub:
-            return
-        out = Image()
-        out.header = copy.copy(msg.header)
-        out.header.stamp = self.get_clock().now().to_msg()
-        out.header.frame_id = msg.header.frame_id
+    def frame_combine(slef,frame, src):
+        if len(frame.shape) == 3:
+            frameH, frameW = frame.shape[:2]
+            srcH, srcW = src.shape[:2]
+            dst = np.zeros((max(frameH, srcH), frameW + srcW, 3), np.uint8)
+            dst[:, :frameW] = frame[:, :]
+            dst[:, frameW:] = src[:, :]
+        else:
+            src = cv.cvtColor(src, cv.COLOR_BGR2GRAY)
+            frameH, frameW = frame.shape[:2]
+            imgH, imgW = src.shape[:2]
+            dst = np.zeros((frameH, frameW + imgW), np.uint8)
+            dst[:, :frameW] = frame[:, :]
+            dst[:, frameW:] = src[:, :]
+        return dst
 
-        out.height       = msg.height
-        out.width        = msg.width
-        out.encoding     = msg.encoding
-        out.is_bigendian = msg.is_bigendian
-        out.step         = msg.step
-        out.data         = msg.data  
-        
-        self.pub.publish(out)
-        self._next_pub = now + self._period
-def main(args=None):
-    rclpy.init(args=args)
-    node = CameraInfo()
-    rclpy.spin(node)
-    node.destroy_node()
-    rclpy.shutdown()
-    
 if __name__ == '__main__':
-    main()
+    rospy.init_node('handDetector', anonymous=True)
+    capture = cv.VideoCapture(4)
+    capture.set(6, cv.VideoWriter.fourcc('M', 'J', 'P', 'G'))
+    capture.set(cv.CAP_PROP_FRAME_WIDTH, 640)
+    capture.set(cv.CAP_PROP_FRAME_HEIGHT, 480)
+    capture.set(cv.CAP_PROP_FPS, 60)
+    print("capture get FPS : ", capture.get(cv.CAP_PROP_FPS))
+    pTime = cTime = 0
+    hand_detector = HandDetector(maxHands=2)
+    while capture.isOpened():
+        ret, frame = capture.read()
+        # frame = cv.flip(frame, 1)
+        frame, img = hand_detector.pubHandsPoint(frame, draw=False)
+        if cv.waitKey(1) & 0xFF == ord('q'): break
+        cTime = time.time()
+        fps = 1 / (cTime - pTime)
+        pTime = cTime
+        text = "FPS : " + str(int(fps))
+        cv.putText(frame, text, (20, 30), cv.FONT_HERSHEY_SIMPLEX, 0.9, (0, 0, 255), 1)
+        dist = hand_detector.frame_combine(frame, img)
+        cv.imshow('dist', dist)
+        #cv.imshow('frame', frame)
+        # cv.imshow('img', img)
+    capture.release()
+    cv.destroyAllWindows()
