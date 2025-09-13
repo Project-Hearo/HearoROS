000d5504f084b1e1da5ca7c12bca8fc26540415f
rkrzy <81816761+rkrzy@users.noreply.github.com>
2025-09-14 02:54:45 +0900

    feat: 해상도 변경 (#5)

--- src/service_controller/service_controller/camera_stream.py
+++ src/service_controller/service_controller/camera_stream.py
@@ -14,8 +14,8 @@ class RtspStream(Node):
         super().__init__('rtsp_stream')
         self.declare_parameters('', [
             ('device', '0'),                 
-            ('width', 640),
-            ('height', 480),
+            ('width', 320),
+            ('height', 240),
             ('fps', 120),
             ('camera_fourcc', 'MJPG'),       
             ('publish_topic', '/stream_image_raw'),
